573c2b662a9076069aab9a15f1272ad8672c5f2e
dfireBird <me@dfirebird.dev>
2023-01-02 23:22:54 +0530

    fix: change incompatible types used in db models refer: https://github.com/diesel-rs/diesel/discussions/3388

--- protin-backend/src/models.rs
+++ protin-backend/src/models.rs
@@ -1,12 +1,12 @@
-use diesel::{
-    prelude::*,
-    sql_types::{Timestamp, Uuid},
-};
+use std::time::SystemTime;
+
+use diesel::prelude::*;
+use uuid::Uuid;
 
 #[derive(Queryable)]
 pub struct Paste {
     pub id: Uuid,
     pub file: String,
-    pub created_at: Timestamp,
-    pub expires_at: Timestamp,
+    pub created_at: SystemTime,
+    pub expires_at: SystemTime,
 }
