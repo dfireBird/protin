af56009d32b2e3f6d2c6c3a64904f0f885dc3281
dfireBird <me@dfirebird.dev>
2025-04-19 18:14:14 +0530

    fix: set theme colors to single variable on dark mode using css

--- protin-frontend/app/components/navbar.tsx
+++ protin-frontend/app/components/navbar.tsx
@@ -6,14 +6,14 @@ type PageType = "new" | "paste";
 
 export function NavBar({ pageType }: { pageType: PageType }) {
   return (
-    <nav className="sm:items-stretch flex items-center justify-between px-2 py-1 text-xl sm:px-6 lg:px-8 border-b-2 border-foreground/5">
+    <nav className="flex items-center justify-between px-2 pb-2 pt-1 text-xl md:px-6 lg:px-8 2xl:px-10 border-b-1 border-foreground/10">
       <div>
         <NavLink to={"/"}>
           <h1 className="font-medium text-foreground">Protin</h1>
         </NavLink>
       </div>
       <div>
-        <Button>{pageType == "new" ? "Save" : "New"}</Button>
+        <Button>{pageType === "new" ? "Save" : "New"}</Button>
       </div>
     </nav>
   );
