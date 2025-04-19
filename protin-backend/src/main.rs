fe282e0a2c39f59c05c3a986be69f400867ed8e6
dfireBird <me@dfirebird.dev>
2025-04-20 01:46:29 +0530

    fix: remove the unsafe function of `env::set_var`

--- protin-backend/src/main.rs
+++ protin-backend/src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 
 use anyhow::Context;
 use dotenvy;
+use env_logger::Env;
 
 use protin::Config;
 
@@ -10,18 +11,8 @@ async fn main() -> anyhow::Result<()> {
     dotenvy::from_path(env::var("ENV_FILE").unwrap_or("../.env".to_string()))
         .context(".env file not found.")?;
 
-    if env::var("RUST_LOG").ok().is_none() {
-        unsafe {
-            /*
-             * The function is unsafe only on multi-threaded programs on OS other than Windows.
-             * Tho this the web server is multi-threaded program, at this point the threads are
-             * spawned yet. So I think it's mostly safe to use.
-             */
-            // FIXME: find a different way to do this
-            env::set_var("RUST_LOG", "protin=debug,actix_web=info");
-        }
-    }
-    env_logger::init();
+    let logger_env = Env::default().default_filter_or("protin=debug,actix_web=info");
+    env_logger::init_from_env(logger_env);
 
     let config = Config::from_env()?;
 
