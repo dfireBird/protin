33c4981c53ac07ae71c71422d0dd7412e6f54495
dfireBird <me@dfirebird.dev>
2025-04-20 23:21:38 +0530

    feat: implement view paste page

---
+++ protin-frontend/app/routes/paste.tsx
@@ -0,0 +1,47 @@
+import { useNavigate } from "react-router";
+
+import { useToolbar } from "~/providers/toolbar";
+
+import type { Route } from "./+types/paste";
+import { useEffect } from "react";
+
+export async function loader({ params: { pasteId } }: Route.LoaderArgs) {
+  const url = process.env.API_URL || "";
+  const resp = await fetch(`${url}/api/paste/${pasteId}`);
+  if (resp.ok) {
+    const content = await resp.text();
+    return { content };
+  }
+  {
+    // fixme: handle errors from resp
+    return { error: "Error" };
+  }
+}
+
+export default function Paste({ loaderData }: Route.ComponentProps) {
+  if (loaderData.error) {
+    // handle errors
+    return;
+  }
+
+  const navigate = useNavigate();
+  const { setToolbarState } = useToolbar();
+
+  const onClickHandler = () => navigate("/");
+
+  useEffect(() => {
+    setToolbarState({ state: "new", action: onClickHandler });
+  }, []);
+
+  const content = loaderData.content;
+  return (
+    <div className="h-full flex">
+      <div className="line h-full p-2 pb-0 border-r-1 border-border/10 text-md font-medium">
+        {">"}
+      </div>
+      <pre className="font-mono h-full p-2 pb-0 flex-auto text-md font-medium">
+        <code>{content}</code>
+      </pre>
+    </div>
+  );
+}
